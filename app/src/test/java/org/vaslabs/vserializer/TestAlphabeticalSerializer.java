4817b22c6c0817e9eff9f49091339e10ee96443e
Vasilis Nicolaou <vaslabs@yahoo.gr>
2016-05-02 20:31:42 +0100

    Compare jvm with this

--- app/src/test/java/org/vaslabs/vserializer/TestAlphabeticalSerializer.java
+++ app/src/test/java/org/vaslabs/vserializer/TestAlphabeticalSerializer.java
@@ -3,10 +3,15 @@ package org.vaslabs.vserializer;
 
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.nio.ByteBuffer;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
@@ -70,11 +75,45 @@ public class TestAlphabeticalSerializer {
         assertEquals(5, cds.somethingElse.b);
     }
 
+    @Test
+    public void measureDifferenceFromVM() {
+        VSerializer serializer = new AlphabeticalSerializer();
+        ComplexDataStructure cds = new ComplexDataStructure();
+        cds.a = 0xff;
+        cds.b = -1;
+        cds.somethingElse = new ComplexDataStructure();
+        cds.somethingElse.a = -2;
+        cds.somethingElse.b = 5;
+        byte[] data = serializer.serialize(cds);
+
+        byte[] jvmData = serializeObject(cds);
+
+        System.out.println("VSerializer: " + data.length);
+        System.out.println("JVM Serializer: " + jvmData.length);
+        assertTrue(data.length < jvmData.length);
+    }
+
     private void initWithData(EncapsulatedData myTestObject) {
         myTestObject.a = 0xff121212;
         myTestObject.b = 0x1111;
         myTestObject.c = 0xf;
         myTestObject.d = 0xff;
+    }
+
+    private byte[] serializeObject(ComplexDataStructure cds) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        byte[] data = null;
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(baos);
+            oos.writeObject(cds);
+            data = baos.toByteArray();
+            oos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return data;
+
     }
 
 
@@ -85,7 +124,7 @@ public class TestAlphabeticalSerializer {
         private byte c;// = 0xf;
     }
 
-    public static class ComplexDataStructure {
+    public static class ComplexDataStructure implements Serializable {
         private long a;
         private int b;
         private ComplexDataStructure somethingElse;
