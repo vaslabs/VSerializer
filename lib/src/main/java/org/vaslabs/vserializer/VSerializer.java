1608ada5845ebef2a4692c98c6203ad538cf5ab2
Vasilis Nicolaou <vaslabs@yahoo.gr>
2016-06-09 19:32:17 +0100

    Support for lists and various fixes

--- lib/src/main/java/org/vaslabs/vserializer/VSerializer.java
+++ lib/src/main/java/org/vaslabs/vserializer/VSerializer.java
@@ -1,5 +1,7 @@
 package org.vaslabs.vserializer;
 
+import java.util.List;
+
 /**
  * Created by vnicolaou on 02/05/16.
  */
@@ -7,7 +9,12 @@ public interface VSerializer {
 
     <T> byte[] serialize(T obj);
 
+    <T> byte[] serialize(List<T> list);
+
     <T> byte[] serialize(T[] objects);
 
     <T> T deserialise(byte[] data, Class<T> clazz);
+
+
+    <T> List<T> deserialise(byte[] data, Class<List> listClass, Class<T> parametarizedClass);
 }
